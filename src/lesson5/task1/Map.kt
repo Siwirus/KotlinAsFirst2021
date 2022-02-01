fa471bf7880f4521eaea06e99e717cc94e293ec3
Siwirus <kotyukov.iv@edu.spbstu.ru>
2022-02-01 23:37:49 +0300

    Update Map.kt

--- src/lesson5/task1/Map.kt
+++ src/lesson5/task1/Map.kt
@@ -119,13 +119,7 @@ fun buildGrades(grades: Map<String, Int>): Map<Int, List<String>> = TODO()
  *   containsIn(mapOf("a" to "z"), mapOf("a" to "z", "b" to "sweet")) -> true
  *   containsIn(mapOf("a" to "z"), mapOf("a" to "zee", "b" to "sweet")) -> false
  */
-fun containsIn(a: Map<String, String>, b: Map<String, String>): Boolean {
-    for ((key, value) in b) {
-        if (a[key] == value) return true
-    }
-    return false
-}
-
+fun containsIn(a: Map<String, String>, b: Map<String, String>): Boolean = TODO()
 
 /**
  * Простая (2 балла)
